3068370c9e427a6834ce725783214fdbeb504724
ELDRON64 <rethernet@gmail.com>
2026-07-22 20:41:16 +0200

    weapon

---
+++ components/Weapons.cpp
@@ -0,0 +1,12 @@
+#include <engine.hpp>
+
+class Weapons : public Behaviour
+{
+private:
+	/* data */
+public:
+	Weapons ( )
+	{
+		Informations = { "Weapons", 1.0, "This is the weapons information" };
+	}
+};
